4fd4f36c0b69ec65802d8432d7a6c290da96b6b2
jarask <jrask96@gmail.com>
2022-04-07 12:29:37 +0200

    Small adjustments

--- knn_class_test.py
+++ knn_class_test.py
@@ -1,10 +1,8 @@
 import knn_decoding
 import numpy as np
 
-
-ts_name = 'tsoutput_p1_sigma2_amp5_bkg0'
-ts_path = 'Data/'
-knn_decoder = knn_decoding.kNNDecoder('Data/p1_sigma2/a5_p1_bkg0/tsoutput.csv',
-                                      'Codebook.csv', 16, 1, verbose=True)
-df = knn_decoder.run_knn_decoding()
-df.to_csv('Data/p1_sigma2/a5_p1_bkg0/knn_decoding.csv')
+if __name__ == '__main__':
+    knn_decoder = knn_decoding.kNNDecoder('data/p1_sigma2/a5_p1_bkg100/tsoutput.csv',
+                                          'Codebook.csv', 16, 1, verbose=True)
+    df = knn_decoder.run_knn_decoding()
+    # df.to_csv('data/p1_sigma2/a5_p1_bkg100/knn_decoding.csv')
