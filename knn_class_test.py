a400d07d7731b409cd956490986a7958515a3c10
jarask <jrask96@gmail.com>
2022-05-26 09:57:39 +0200

    Update knn_class_test.py

--- knn_class_test.py
+++ knn_class_test.py
@@ -1,8 +1,11 @@
-import knn_decoding
-import numpy as np
+from kNNDecoder import knn_decoding
+import compare_decoding
 
 if __name__ == '__main__':
-    knn_decoder = knn_decoding.kNNDecoder('data/p1_sigma2/a5_p1_bkg100/tsoutput.csv',
+    knn_decoder = knn_decoding.kNNDecoder('data/p5_sigma2/a5_p5_bkg0/tsoutput.csv',
                                           'Codebook.csv', 16, 1, verbose=True)
     df = knn_decoder.run_knn_decoding()
-    # df.to_csv('data/p1_sigma2/a5_p1_bkg100/knn_decoding.csv')
+    df.to_csv('data/p5_sigma2/a5_p5_bkg0/knn_decoding.csv')
+
+    compare_decoding.compare_decoding(amplitude=5, points_per_gene=5,
+                                      bkg=0, sigma=2, search_tolerance=3)
